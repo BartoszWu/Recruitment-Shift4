8d4620587e9fb21d8f10ce9b2c0cae71e8e4aeef
bartosz_wisniewski <bartosz-wisniewski@outlook.com>
2024-05-23 14:05:21 +0200

    Add first version of Input and MoneyInput component

---
+++ src/components/input/MoneyInput.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import type { Meta, StoryObj } from '@storybook/react'
+import { MoneyInput } from './MoneyInput'
+const meta = {
+    title: 'Components/MoneyInput',
+    component: MoneyInput,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen',
+    },
+} satisfies Meta<typeof MoneyInput>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Basic = {
+    render: () => {
+        const [value, onChange] = React.useState('')
+        const handleOnChange = (newVal: string) => {
+            onChange(newVal)
+        }
+
+        return (
+            <>
+                <div>'1,234.56'</div>
+                <MoneyInput value={value} onChange={handleOnChange} />
+            </>
+        )
+    },
+}
