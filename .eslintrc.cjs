e81b6b4781f316c5f1ce9284a4b421e6869ce767
bartosz_wisniewski <bartosz-wisniewski@outlook.com>
2024-05-23 14:05:06 +0200

    Add storybook and update configuration of project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,7 @@
 module.exports = {
     root: true,
     env: { browser: true, es2020: true },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended-type-checked',
-        'plugin:@typescript-eslint/stylistic-type-checked',
-        'plugin:react/recommended',
-        'plugin:react/jsx-runtime',
-        'plugin:react-hooks/recommended',
-        'plugin:jsx-a11y/recommended',
-        'prettier',
-    ],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked', 'plugin:@typescript-eslint/stylistic-type-checked', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:react-hooks/recommended', 'plugin:jsx-a11y/recommended', 'prettier', 'plugin:storybook/recommended'],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
@@ -21,6 +12,7 @@ module.exports = {
     },
     plugins: ['react-refresh', 'jsx-a11y'],
     rules: {
+        '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
         'react-refresh/only-export-components': [
             'warn',
             { allowConstantExport: true },
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import { MoneyInput } from './components/input/moneyInput'
 
 function App() {
     return (
         <>
-            <h1 className="text-3xl">Hello Word</h1>
+            <MoneyInput />
         </>
     )
 }
